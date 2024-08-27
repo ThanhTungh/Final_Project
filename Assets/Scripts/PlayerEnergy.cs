fcb8d210a08d18d560afe41e518ead8da7011d6f
ThanhTungh <tungtottinh555@gmail.com>
2024-08-27 17:27:27 +0700

    update character Recover energy and using energy

---
+++ Assets/Scripts/PlayerEnergy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEnergy : MonoBehaviour
+{
+    [Header("Player")]
+    [SerializeField] private PlayerConfig playerConfig;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            UseEnergy(1f);
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            RecoverEnergy(1f);
+        }
+    }
+    public void UseEnergy(float amount)
+    {
+        playerConfig.CurrentEnergy -= amount;
+        if (playerConfig.CurrentEnergy < 0)
+        {
+            playerConfig.CurrentEnergy = 0;
+        }
+    }
+    public void RecoverEnergy(float amount)
+    {
+        playerConfig.CurrentEnergy += amount;
+        if (playerConfig.CurrentEnergy > playerConfig.MaxEnergy)
+        {
+            playerConfig.CurrentEnergy = playerConfig.MaxEnergy;
+        }
+    }
+}
