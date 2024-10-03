b3764f8f666fbd84d4f705f0d249fa9a7b4881aa
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2024-10-03 17:34:30 +0700

    create folder "Selectable Players"

---
+++ Assets/Scripts/Menu/SelectablePlayer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectablePlayer : MonoBehaviour
+{
+    [SerializeField] private PlayerConfig config;
+    public PlayerConfig Config => config;
+
+    private void OnMouseDown() {
+        
+    }
+}
