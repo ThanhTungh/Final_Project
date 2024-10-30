33be299f95d1f2d71257ec7b0eacb6e12ca9f1a1
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2024-10-30 16:25:37 +0700

    "Enemy" attack "Player": action + animation

--- Assets/Scripts/Enemy/Actions/ActionAttack.cs
+++ Assets/Scripts/Enemy/Actions/ActionAttack.cs
@@ -2,8 +2,32 @@ using UnityEngine;
 
 public class ActionAttack : FSMAction
 {
+    [Header("Config")]
+    [SerializeField] private float timeBtwAttacks;
+
+    private EnemyBrain enemy;
+    private EnemyWeapon enemyWeapon;
+    private float attackTimer;
+
+    private void Awake()
+    {
+        enemy = GetComponent<EnemyBrain>();
+        enemyWeapon = GetComponent<EnemyWeapon>();
+    }
+
+    private void Start() 
+    {
+        attackTimer = timeBtwAttacks;
+    }
+
     public override void Act()
     {
-        Debug.Log("Attack!!!!!");
+        if (enemy.Player == null) return;
+        attackTimer -= Time.deltaTime;
+        if (attackTimer <= 0f)
+        {
+            enemyWeapon.UseWeapon();
+            attackTimer = timeBtwAttacks;
+        }
     }
 }
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
+public class PlayerHealth : MonoBehaviour, ITakeDamage
 {
     [Header("Player")]
     [SerializeField] private PlayerConfig playerConfig;
